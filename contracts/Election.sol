ba032a96dd277525c63889fb0c716023b6427a86
dotm <desde.el.mar96@gmail.com>
2018-09-29 19:45:42 +0700

    3. Cast Votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,9 @@ contract Election {
         uint voteCount;
     }
 
+    // Store accounts that have voted
+    mapping(address => bool) public voters;
+
     // Store Candidates
     // Fetch Candidate
     mapping(uint => Candidate) public candidates;
@@ -24,4 +27,14 @@ contract Election {
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 
+    function vote (uint _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender], "address has already vote");
+         // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount, "invalid candidateId");
+         // record that voter has voted
+        voters[msg.sender] = true;
+         // update candidate vote Count
+        candidates[_candidateId].voteCount ++;
+    }
 }
